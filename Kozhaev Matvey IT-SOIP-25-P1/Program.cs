1f3a3089725b06d3c7cfac551a25f64c2ca8ba1e
matve <matve@DESKTOP-GUJNR4A>
2026-09-24 13:55:28 +1000

    Добавьте файлы проекта.

---
+++ Kozhaev Matvey IT-SOIP-25-P1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kozhaev_Matvey_IT_SOIP_25_P1
+{
+    abstract class Delivery
+    {
+        protected string name;
+        protected decimal price;
+        public Delivery(string name)
+        {
+            Name = name;
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value) && value.Length <= 20)
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! имя не может быть пустым и должно быть не длинее 20 символов");
+                    name = "Неизвестно";
+                }
+            }
+        }
+        public decimal Price
+        {
+            get { return price; }
+        }
+        public abstract void CalculatePrice(decimal basePrice);
+        public override string ToString()
+        {
+            return $"Доставка: {Name} - Стоимость: {Price} рублей";
+        }
+    }
+    class StandardDelivery : Delivery
+    {
+        public StandardDelivery(string name) : base(name)
+        {
+        }
+        public override void CalculatePrice(decimal basePrice)
+        {
+            price = basePrice * 1.0m;
+        }
+    }
+    class ExpressDelivery : Delivery
+    {
+        public ExpressDelivery(string name) : base(name)
+        {
+        }
+        public override void CalculatePrice(decimal basePrice)
+        {
+            price = basePrice * 2.5m;
+        }
+    }
+    class InternationalDelivery : Delivery
+    {
+        public InternationalDelivery(string name) : base(name)
+        {
+        }
+        public override void CalculatePrice(decimal basePrice)
+        {
+            price = basePrice * 4.0m;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal basePrice = 20m;
+            Delivery standard = new StandardDelivery("Стандартная");
+            Delivery express = new ExpressDelivery("Экспресс");
+            Delivery international = new InternationalDelivery("Международная");
+            standard.CalculatePrice(basePrice);
+            express.CalculatePrice(basePrice);
+            international.CalculatePrice(basePrice);
+            Console.WriteLine("Информация о доставках");
+            Console.WriteLine(standard);
+            Console.WriteLine(express);
+            Console.WriteLine(international);
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Kozhaev Matvey IT-SOIP-25-P1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Kozhaev Matvey IT-SOIP-25-P1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kozhaev Matvey IT-SOIP-25-P1")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("e73e764a-9368-4702-a8a3-bcc7b88cd9a4")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
